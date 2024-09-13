c0839b16188f048bc777fae5f86c53ceb6a9848e
Daniel Thyselius <daniel@mindfulstack.se>
2024-09-13 11:05:49 +0200

    Changes during day 4

--- Demo.Api/Program.cs
+++ Demo.Api/Program.cs
@@ -1,6 +1,7 @@
 using Demo.Api;
 using DemoModels;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 internal class Program
 {
@@ -11,7 +12,16 @@ internal class Program
         // Add services to the container.
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new()
+            {
+                Title = "Vårt fina Demo Api",
+                Version = "v1",
+                Description = "Ett fint Api",
+                Contact = new() { Name = "Daniel", Email = "..." }
+            });
+        });
 
         // Add our services to the container.
         builder.Services.AddSingleton<IBookService, BookService>();
@@ -30,23 +40,29 @@ internal class Program
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
-            app.UseSwaggerUI();
-            app.UseCors("allowAll");
+            app.UseSwaggerUI(options =>
+            {
+                options.DocExpansion(DocExpansion.List);
+                options.DefaultModelsExpandDepth(-1);
+            });
+            //app.UseCors("allowAll");
         }
 
         app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.MapGet("/", () => Results.File("./local-test-page.html", "text/html"));
 
         // Map endopoints
-        app.MapGet("/books", GetAllBooks);
+        app.MapGet("/books", GetAllBooks)
+            .WithSummary("Hämtar alla böcker")
+            .WithDescription("Hämtar alla böcker som sagt...")
+            .WithTags("Böcker")
+            .Produces<Book>(StatusCodes.Status200OK);
+            
         app.MapGet("/books/{id}", GetBookById);
         app.MapPut("/books/{id}", UpdateBook);
-        app.MapPost("/books", (Book book, IBookService bookService) =>
-        {
-            // TODO: Flytta detta till en egen metod
-            // Problem med formulär? Lägg till [FromForm] i parametern ex: ([FromForm] Book book)
-            bookService.Add(book);
-            return Results.Created($"/books/{book.Id}", book);
-        });
+        app.MapPost("/books", CreateBook);
 
 
         app.Run();
@@ -55,7 +71,16 @@ internal class Program
 
 
     // Handlers
-    private static IResult UpdateBook(int id, Book book, IBookService bookService)
+
+    static IResult CreateBook(Book book, IBookService bookService)
+    {
+        // TODO: Flytta detta till en egen metod
+        // Problem med formulär? Lägg till [FromForm] i parametern ex: ([FromForm] Book book)
+        bookService.Add(book);
+        return Results.Created($"/books/{book.Id}", book);
+    }
+
+    static IResult UpdateBook(int id, Book book, IBookService bookService)
     {
         var existingBook = bookService.GetBookById(id);
 
